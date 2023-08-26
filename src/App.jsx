0520e4e22ed074cde88133ad4a8907bb1ce4a5b7
TropicolX <jacobsbusayo@gmail.com>
2023-08-26 17:26:23 +0100

    starter template

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useEffect, useRef, useState } from "react";
-
 import Planet from "./components/Planet";
 import Stars from "./components/Stars";
 
@@ -99,39 +97,6 @@ const planetsInSolarSystem = [
 const limit = 30;
 
 function App() {
-	const [planets, setPlanets] = useState(planetsInSolarSystem);
-	const [offset, setOffset] = useState(0);
-	const loaderRef = useRef(null);
-
-	const loadMorePlanets = async () => {
-		try {
-			const response = await fetch(
-				`https://planets-api-rho.vercel.app/api/planets?offset=${offset}`
-			);
-			const data = await response.json();
-			setPlanets([...planets, ...data]);
-			setOffset((previousOffset) => previousOffset + limit);
-		} catch (error) {
-			console.error(error);
-		}
-	};
-
-	useEffect(() => {
-		const observer = new IntersectionObserver((entries) => {
-			const firstEntry = entries[0];
-			if (firstEntry.isIntersecting) {
-				// Load more planets when the loader is visible
-				loadMorePlanets();
-			}
-		});
-		if (loaderRef.current) {
-			observer.observe(loaderRef.current);
-		}
-
-		// Clean up the observer on component unmount
-		return () => observer.disconnect();
-	}, [offset]);
-
 	return (
 		<div className="universe">
 			<div className="stars">
@@ -139,12 +104,6 @@ function App() {
 			</div>
 			<h1 className="title">Explore the Universe</h1>
 			<p className="subheading">Journey through the cosmos</p>
-			<div className="planets">
-				{planets.map((planet) => (
-					<Planet key={planet.name} data={planet} />
-				))}
-			</div>
-			<div ref={loaderRef} className="spinner"></div>
 		</div>
 	);
 }
